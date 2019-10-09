bd2e605551a688f8968bddf9feced3a29d5ba9fc
Yimeng <yimeng.yu@gmail.com>
2019-10-08 23:38:14 -0700

    update index.html style.css, js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// set a list of word
+var wordlist = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+// The computer randomly pick a word
+var word = word[Math.floor(Math.random()*wordlist.length)];
+
+var playerInputArray = [];
+for (var i=0; i <word.lengh;i++) {
+    anserArray[i]="_";
+}
+var remainingLetters = word.lengh;
+while (remainingLetters > 0) {
+    console.log(anserArray.jion(""));
+
+    var 
+}
